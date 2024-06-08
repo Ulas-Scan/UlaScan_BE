eda89b439aee341a5b249c1865407f712c9c14e0
Java Kanaya Prada <javakanaya@gmail.com>
2024-06-09 00:36:45 +0700

    fix: more prompt update

--- constants/prompt.go
+++ constants/prompt.go
@@ -16,12 +16,20 @@ Output:
 	Pastikan bahwa total jumlah komentar tidak melebihi 100, tidak ada yang tumpang tindih, dan setiap komentar hanya diklasifikasikan ke satu kategori. Hanya perlu menampilkan jumlah komentar tanpa rincian komentar aslinya:
 jumlah = (komentar positif / (komentar positif + komentar negatif)) * 100
 jumlah dalam format float atau bilangan desimal.
-Output JSON dengan format sebagai berikut:
+
+PENTING: Output harus berupa JSON yang menyimpan score dalam bentuk desimal atau float bukan string dengan format sebagai berikut:   
 {
 	"packaging": float(jumlah),
 	"delivery": float(jumlah),
 	"admin_response": float(jumlah),
 	"product_condition": float(jumlah)
+}                                              
+Contoh Output:
+{
+	"packaging": 12.34,
+	"delivery": 56.78,
+	"admin_response": 91.01,
+	"product_condition": 11.12
 }
 `
 )
